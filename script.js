76ca498943ef82de1307ead1d383103a68f667f5
LikeKey <alehasidorovi@gmail.com>
2021-04-27 20:25:46 +0900

    Callback functions

--- script.js
+++ script.js
@@ -1,63 +1,26 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-function rememberMyMovies() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Один из последних просмотренных фильмов?", "");
-        
-        while (a === "" || a === null || a.length > 50) {
-            a = prompt("Один из последних просмотренных фильмов?", "");
-        }
-    
-        const b = +prompt("На сколько оцените его?", "");
-        
-        personalMovieDB.movies[a] = b;
-    }
+function second() {
+    console.log(2);
 }
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
+// first();
+// second();
+
+function learnJavaScript(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-function showMyDB() {
-    if (!personalMovieDB.private) {
-        console.log(personalMovieDB);
-    }
+function done() {
+    console.log("Я прошел этот урок!");
 }
 
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
-    }
-}
-
-start();
-rememberMyMovies();
-writeYourGenres();
-detectPersonalLevel();
-showMyDB();
+learnJavaScript("JS", done);
