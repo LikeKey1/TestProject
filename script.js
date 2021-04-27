f256ae8e576991a12f65da76d943f53f02e608df
LikeKey <alehasidorovi@gmail.com>
2021-04-27 20:15:14 +0900

    Added new functions to the first init

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 'use strict';
 
-const str = "tESt";
-const arr = [1,2,3];
+let numberOfFilms;
 
-console.log(arr.length);
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf("fruit"));
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
 
-const logg = "Hello world";
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
-console.log(logg.slice(6, 11));
+function rememberMyMovies() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Один из последних просмотренных фильмов?", "");
+        
+        while (a === "" || a === null || a.length > 50) {
+            a = prompt("Один из последних просмотренных фильмов?", "");
+        }
+    
+        const b = +prompt("На сколько оцените его?", "");
+        
+        personalMovieDB.movies[a] = b;
+    }
+}
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+function showMyDB() {
+    if (!personalMovieDB.private) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
+    }
+}
+
+start();
+rememberMyMovies();
+writeYourGenres();
+detectPersonalLevel();
+showMyDB();
