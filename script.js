774f03fae0db47bb6a6ac03e562f1bf1e007a01b
LikeKey <alehasidorovi@gmail.com>
2021-04-27 19:24:38 +0900

    A little bit about functions

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const number = 20;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Один из последних просмотренных фильмов?", "");
-    
-    while (a === "" || a === null || a.length > 50) {
-        a = prompt("Один из последних просмотренных фильмов?", "");
-    }
-
-    const b = +prompt("На сколько оцените его?", "");
-    
-    personalMovieDB.movies[a] = b;
+function showFirstMessage(text) {
+    console.log(text);
+    const number = 10;
+    console.log(number);
 }
 
-console.log(personalMovieDB);
+showFirstMessage("Hi man");
+console.log(number);
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function calc(a, b) {
+    return (a + b);
 }
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, -11));
