5f277fdba96eb834da1e214eb340b910c0e7b1a9
LikeKey <alehasidorovi@gmail.com>
2021-04-29 18:00:57 +0900

    Objects and a destructions of objects

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 'use strict';
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        background: "red"
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+
+for (let key in options) {
+    if (typeof(options[key]) === "object") {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
 }
 
-function second() {
-    console.log(2);
-}
+console.log(Object.keys(options).length);
 
-// first();
-// second();
-
-function learnJavaScript(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log("Я прошел этот урок!");
-}
-
-learnJavaScript("JS", done);
+const {border, background} = options.colors;
+console.log(border);
